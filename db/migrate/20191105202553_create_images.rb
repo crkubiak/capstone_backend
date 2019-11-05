2a36c79afdc34d22176c51f6571eb75e90a18a0b
crkubiak <charles@8thlight.com>
2019-11-05 17:34:26 -0600

    model corrected for images

--- db/migrate/20191105202553_create_images.rb
+++ db/migrate/20191105202553_create_images.rb
@@ -1,7 +1,10 @@
 class CreateImages < ActiveRecord::Migration[6.0]
   def change
     create_table :images do |t|
-      t.image_url
+      t.string :image_url
+      
+      t.references :records
+      
       t.timestamps
     end
   end
