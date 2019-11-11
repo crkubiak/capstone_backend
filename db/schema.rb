b6ad983f950bcbdf7675442fcd82a71e9e43fd99
crkubiak <charles@8thlight.com>
2019-11-10 18:45:39 -0600

    [MIGRATION] removed images table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_194724) do
+ActiveRecord::Schema.define(version: 2019_11_11_004349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,14 +34,6 @@ ActiveRecord::Schema.define(version: 2019_11_08_194724) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "images", force: :cascade do |t|
-    t.string "image_url"
-    t.bigint "record_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["record_id"], name: "index_images_on_record_id"
   end
 
   create_table "records", force: :cascade do |t|
